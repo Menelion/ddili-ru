b28150162b6568836bb903d5810f5a36af2dc90f
Andre Polykanine <andre@oire.org>
2015-12-26 22:51:44 +0200

    Adding the English sources; started Hello World translation

---
+++ src/ders/index.d
@@ -0,0 +1,20 @@
+Ddoc
+
+$(H5 Dersler)
+
+$(UL
+$(LI $(LINK2 /ders/d/index.html, D Programlama Dili), $(I Ali Çehreli))
+$(LI $(LINK2 /ders/gtkd/index.html, GtkD ile Görsel Programlama), $(I Mengü Kağan ve Can Alpay Çiftçi))
+$(LI $(LINK2 /ders/sdl/index.html, SDL ile Oyun Programlama), $(I Faruk Erdem Öncel))
+$(LI $(LINK2 /ders/d.en/index.html, Programming in D), $(I Ali Çehreli))
+$(LI $(LINK2 /ders/d.ru/index.html, программирование на языке D (Russian translation of Programming in D), $(I али Чехрели (Ali Çehreli)), Russian translation $(I Andre Polykanine and Russian translation team))
+)
+
+Macros:
+        SUBTITLE=D Dersleri
+
+        DESCRIPTION=D programlama dili dersleri
+
+        KEYWORDS=d programlama dili d2 dersleri öğrenmek tutorial
+
+        BREADCRUMBS=$(BREADCRUMBS_INDEX)
